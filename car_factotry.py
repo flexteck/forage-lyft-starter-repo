55ff148b412cdda08746fcaa13a4bf988b75a99f
Anthony kanu <andrewandrenzanthony@gmail.com>
2023-12-04 21:26:31 +0100

    new update on car_factotry file

--- car_factotry.py
+++ car_factotry.py
@@ -9,9 +9,18 @@ from car import Car
 
 class CarFactory(Car):
     """ creates cars """
-
+    @staticmethod
     def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
         """ creates a Calliope car """
         engine = CapuletEngine(last_service_mileage, current_mileage)
-        battery = NubbinBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        battery = SpindlerBattery(last_service_date, current_date)
+        car = Car(engine, battery)
+        return car
+    
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        """ creates a Glissade car """
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        car = Car(engine, battery)
+        return car
